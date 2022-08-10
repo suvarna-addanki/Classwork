1f51313537b565a5fb803c5263911580a84b2f0b
Suvarna <suvarna.addanki@gmail.com>
2022-08-10 16:24:44 -0400

    variables&conditionals

---
+++ practiceJS(Variables_conditionals)/app.js
@@ -0,0 +1,223 @@
+//bad! (not really) (hoisting)
+//var age;
+
+//good!
+//let age;
+
+// constant
+//const age = 8;
+
+//DATA TYPES
+
+//NUMBER
+//const age2 = 10;
+
+//STrings
+//const name = 'shakespeare: "to be or not to be" '
+//const name2 = 'jade'
+//const name3 = 'regg "frhgih" '
+
+//BOOLEAN
+//const employee = true;
+
+//UNDEFINED
+//const example = undefined;
+
+//NULL
+//const example2 = null;
+
+//STRING OPERATOR
+//let firstName = "Bob"
+//let lastName = "The builder"
+
+//concatenation
+//let fullName = firstName + lastName
+//let concatenation = "con" + "cat" + "ena" + "nation"
+
+//console.log(concatenation)
+
+//CAMEL CASE
+//myFavoriteDesert
+
+//PASCEL CASE
+//MyFavoriteDesert
+
+//LOGICAL OPERATORS
+// AND    &&
+//  OR    11
+//  NOT   !
+
+//SHORTHANDS
+
+//let fire = 0;
+
+//fire = fire- 10;
+
+//console.log(fire)
+
+//KEBAB CASE
+//my_favorite_desert
+
+//STRICT Comparison
+//=== (3 equal signs)
+
+//Loose Comparison(uses type Coercion)
+//== (2 equal signs)
+
+//Assignment Operator
+// = (1 equal sign)
+
+//  !==  !=
+
+//let x = 6;
+//let y = 3;
+
+//console.log(x>10 && y>1)
+
+//EXERCISE 1
+
+/*
+let number1 = 10;
+
+let number2 = 12;
+
+let sum = number1 + number2;
+console.log(sum)
+
+//EXERCISE 2
+
+let number3 = 25;
+
+console.log(sum>number3)
+
+//Exercise 3
+let a = 1;
+let b = 2;
+let c = 3;
+let d = 4;
+let e = 5;
+let f = 6;
+let g = 7;
+let h = 8;
+let i = 9;
+let j = 10;
+average1 = (a+b+c+d+e)/5;
+console.log(average1);
+average2 = (f+g+h+i+j)/5;
+console.log(average2);
+average3 = (average1 + average2)/2
+console.log(average3)
+
+//EXERCISE 4
+let num1 = 4;
+let num2 = 10;
+let num3 = 22;
+let num4 = -30;
+let num5 = 55;
+avg1 = (num1 + num2+ num3+ num4 + num5)/5;
+console.log(avg1);
+let num6 = 68;
+let num7 = 7;
+let num8 = -22;
+let num9 = 9;
+let num10 = 100;
+avg2 = (num6 + num7+ num8 + num9 + num10)/5;
+console.log(avg2);
+
+//EXERCISE 5 swapping box 1 and 2 values with each other
+let box1 = "hello"
+let box2 = "class"
+let box3
+box3 = box1;
+box1 = box2;
+box2 = box3;
+
+console.log(box1, box2)
+
+*/
+
+//Exercise  1
+
+/*let num = 3;
+
+if (num > 0) 
+    {
+        console.log("num is positive")
+    
+    }
+    else
+    {
+        console.log("num is negative")
+
+    }
+    */
+    //EXERCISE 2
+
+    /*
+    let age = 20;
+
+    if (age > 18)
+      {
+        console.log("Welcome to the adult site")
+    
+      }else 
+      {
+        console.log("access denied. you need to be 18+ to enter the site")
+      } 
+      */
+
+      // Exercise
+
+      /*let num = 53;
+
+      if(num >0 && num > 100)
+       {
+        console.log("num is positive and greater than 100")
+
+       }
+        elseif(num >0 && num < 100)
+        {
+            console.log("num is positive but lessthan 100")
+        }else
+        
+        {
+        
+            console.log("num is negative")
+
+        }
+        */
+
+        //Exercise 1 (switch statement)
+
+        let letter = "B";
+
+        switch (letter){
+            case 1:
+                if(letter == A )
+                {
+                    console.log("vowel");
+
+                }
+            case 2:
+                if(letter == B)
+                {
+                    console.log("consonant")
+
+                }
+            case 3:
+                if(letter ==C)
+                {
+                    console.log("consonant")
+                }    
+        break;
+
+        }
+        
+        
+
+
+
+
+        
+            
+        
